5116de987aeca7eb824abd1685dd785d5e28faff
R-Rubani <r14@student.douglascollege.ca>
2024-11-05 10:14:16 -0800

    add

---
+++ app/src/main/java/com/example/healthconnectapplication/RegisterActivity.java
@@ -0,0 +1,21 @@
+package com.example.healthconnectapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+            getSupportActionBar().setDisplayUseLogoEnabled(true);
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/healthconnectapplication/WelcomeActivity.java
@@ -0,0 +1,36 @@
+package com.example.healthconnectapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+            getSupportActionBar().setDisplayUseLogoEnabled(true);
+        }
+        TimerTask task = new TimerTask( ) {
+
+            @Override
+            public void run() {
+               finish();
+                startActivity(new Intent(WelcomeActivity.this, RegisterActivity.class));
+            }
+       };
+
+        Timer opening = new Timer( );
+        opening.schedule(task,3000);
+
+    }
+}
